fe8c7456fc4e4c952f6d691c08c4d720db6f1fb5
Stepan <108026663+stepan95@users.noreply.github.com>
2023-05-16 17:14:58 +0300

    Add files via upload

---
+++ language/basic/auth.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    // about.blade
+    'levels' => 'Levels',
+    'statuses' => 'Statuses',
+    'status' => 'Status',
+    'points' => 'Points',
+    'to_next_level' => 'To next level',
+    'to_next_lvl' => 'To next lvl',
+    'exch_rate' => 'Exch. rate',
+    'reload_bonus' => 'Reload bonus',
+    'cashback' => 'Cashback',
+    'birthday_bonus' => 'Birthday bonus',
+    'starter' => 'Starter',
+    'no' => 'No',
+    'expert' => 'Expert',
+    'professional' => 'Professional',
+    'bronze' => 'Bronze',
+    'silver' => 'Silver',
+    'gold' => 'Gold',
+    'platinum' => 'Platinum',
+    'diamond' => 'Diamond',
+    'vip' => 'VIP',
+    'starter' => 'Starter',
+    'specialist' => 'Specialist',
+    'points_from' => 'Points from',
+    'contact_us' => 'contact us',
+    'contact_us_descr' => 'Write us and we will reply as soon as possible',
+    'Send Message' => 'Send Message',
+
+    // blog_details.blade
+    'Latest Blog' => 'Latest Blog',
+    'Write a Comment' => 'Write a Comment',
+    'Most Views' => 'Most Views',
+
+
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+
+    // blog.blade
+    'Read More' => 'Read More',
+
+    // game.blade
+    'All Providers' => 'All Providers',
+
+
+];
---
+++ language/bongo/auth.php
@@ -0,0 +1,245 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+
+    bongo
+    */ 
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    // about.blade
+    'Levels' => 'Levels',
+    'Status' => 'Status',
+    'Points from' => 'Points from',
+    'Reload bonus' => 'Reload bonus',
+    'Cashback' => 'Cashback',
+    'Birthday bonus' => 'Birthday bonus',
+    'about_table_content_1' => 'Points - are accrued units that allow you to raise the level of the player. 1 point is awarded every time when the total bets (accumulative) of the player reach $10. For example, a player bets $0.1, $0.1, $0.5, $0.6, $1, $1.5... and so on! When the amount of bets reaches $10, 1 point is awarded, the counter is reset and the second point is calculated. In the 4th column is set the range of points for moving to the next level.',
+    'about_table_content_2' => 'Cashback - the percentage of return in the form of a bonus from the amount that the player spent (lost). The amount of loss from which each time a cashback is credited as a bonus. For example, the admin set for this level that for every lost (in total) $50, a return in the form of a bonus of 3% of $50. That is, each bet that does not win is summed up and when it reaches the specified value, a bonus is added in the “bonus” cell. In the same place, the balance consists of 2 cells - “money” and “bonus”.',
+    'about_table_content_3' => 'Deposit bonus - the value is set by the admin and this is the percentage of any deposited amount (that is, with each deposit, replenishment of the account), which is credited in the "bonus" cell.',
+    'about_table_content_4' => 'Birthday bonus - a fixed amount of the bonus, is credited to the player in the "bonus" cell on his birthday. If the player does not indicate his birthday, then the bonus is not credited.',
+    'CONTACT US' => 'CONTACT US',
+    'CONTACT US span' => 'Write us and we will reply as soon as possible',
+    'Your Name' => 'Your Name',
+    'Your e-mail' => 'Your e-mail',
+    'Your message' => 'Your message',
+    'Send Message' => 'Send Message',
+
+    // blog_details.blade
+    'Write a Comment' => 'Write a Comment',
+    'Latest Blog' => 'Latest Blog',
+    'Most Views' => 'Most Views',
+
+    // bongocoins.blade
+    'Bongo Coins' => 'Bongo Coins',
+    'bongocoins_hero_subtitle' => 'EARN BONGO COINS WHILE ENJOYING PLAYING IN OUR ONLINE CASINO',
+    'bongocoins_credit_title' => 'Bongo COINS ARE CREDITED AS BONUSES IN THE FOLLOWING CASES',
+    'Welcome bonus' => 'Welcome bonus',
+    'bongocoins_credit_text' => 'Upon registration in the amount of 0.5 Bongo.com',
+    'DEPOSIT BONUS' => 'DEPOSIT BONUS',
+    'With a deposit of' => 'With a deposit of',
+    'bongocoins_credit_text_1' => 'and up to $300 - 5% of the deposit amount',
+    'bongocoins_credit_text_2' => 'and up to $500 - 8% of the deposit amount',
+    'bongocoins_credit_text_3' => 'or more - 10% of the deposit amount',
+    'LOTTERY BONUS' => 'LOTTERY BONUS',
+    'bongocoins_credit_text_4' => 'When buying a lottery ticket 0.5 coins per ticket purchased',
+    'TOURNAMENT BONUS' => 'TOURNAMENT BONUS',
+    'bongocoins_credit_text_5' => 'When participating in tournaments, according to the rules and the prize fund',
+    'bongocoins_what_title' => 'WHAT ARE bongo COINS?',
+    'bongocoins_what_text_1' => 'Bongo coins are bonus user points accumulated in a special balance. The user can exchange these coins in the exchanger for Zino tokens or NFT tokens. You can get bonuses by playing our awesome games, taking part in tournaments and trying your luck in lotteries from the best casino. We strive to be the best online casino in the world and we do everything so that our players have the opportunity to enjoy all the benefits of gambling.',
+    'bongocoins_what_text_2' => 'The ability to exchange Bongo coins will be available to players in early 2023.',
+    'bongocoins_what_title_2' => 'WHAT ARE bongo COINS?',
+    'bongocoins_what_text_3' => 'At the beginning of next year, the ZINO token partnership project will be launched. It will be implemented on a separate site and will be a blockchain system platform.',
+    'bongocoins_what_text_4' => 'Zino tokens are what will enable their owners to be shareholders of the fund, which opens up wide prospects for each participant. Crypto hedge fund from Zino is about liquid cryptocurrencies and real, non-digital assets - futures for gold, currency, securities. This project will include a crypto currency exchange, a wallet, a payment system and other popular services. To appreciate the benefits of Zino tokens, follow our updates!',
+    'bongocoins_what_title_3' => 'WHAT ARE NFT TOKEN?',
+    'bongocoins_what_text_5' => 'NFT is a unique token, it is like a digital entry in the blockchain registry that gives you the rights to own a unique object of the digital space.',
+    'bongocoins_what_text_6' => 'NFT is a type of cryptographic token, each copy of which is unique and cannot be exchanged or replaced by another similar token, although tokens are usually fungible in nature. Each NFT is unique and exists in a single copy, it cannot be divided, and all information about its author, buyer and all transactions with it is securely stored in the blockchain. In other words, an NFT is a digital certificate attached to a unique object. As is the case with any asset, the price of an NFT token for a certain object changes over time, and you may well invest - invest your money in a project at the bottom of the market and sell at the top, thereby earning on the price difference. And we will do our best to help you with this.',
+
+    // bonuses.blade
+    // Думаю що такі речі вже є або будуть реалізовані через адмінку тому цей файл був опущений
+
+
+
+    // commissionLog.blade
+    'Commission From' => 'Commission From',
+    'Commission Level' => 'Commission Level',
+    'Amount' => 'Amount',
+    'Title' => 'Title',
+    'Transaction' => 'Transaction',
+    'Log Not found' => 'Log Not found',
+
+    // contact.blade
+    'Quick Support' => 'Quick Support',
+    'You can get all information' => 'You can get all information',
+    'Call Us' => 'Call Us',
+    'Mail Us' => 'Mail Us',
+    'Visit Us' => 'Visit Us',
+
+    // Copy of tourtament.blade
+    'AWESOME TOURNAMENTS' => 'AWESOME TOURNAMENTS',
+    'START JULY 10!' => 'START JULY 10!',
+    'sign up, and get' => 'sign up, and get',
+    'free' => 'free',
+    'sign up' => 'sign up',
+    'Coming' => 'Coming',
+    'LUCKY 7' => 'LUCKY 7',
+    'Start After' => 'Start After',
+    'prize' => 'prize',
+    'more info' => 'more info',
+    'BIG 21' => 'BIG 21',
+    'Day' => 'Day',
+    'Hour' => 'Hour',
+    'Min' => 'Min',
+    'Sec' => 'Sec',
+
+    // gameLog.blade
+    'Game Name' => 'Game Name',
+    'You Select' => 'You Select',
+    'Result' => 'Result',
+    'Invest' => 'Invest',
+    'Win or Lost' => 'Win or Lost',
+    'Win' => 'Win',
+    'Lost' => 'Lost',
+    'No Data Found' => 'No Data Found',
+
+    // home.blade
+    'hero_content_left_title' => 'FREE 10$',
+    'hero_content_left_subtitle' => 'jUST FOR REGISTRATION',
+
+    // lottery.blade
+    'AWESOME LOTTERIES' => 'AWESOME LOTTERIES',
+    'Coming very soon' => 'Coming very soon',
+    'WHAN WE START!' => 'WHAN WE START!',
+    'INSTANT LOTTERY' => 'INSTANT LOTTERY',
+    'Guess WHO' => 'Guess WHO',
+    'Big prizes' => 'Big prizes',
+    'How to play?' => 'How to play?',
+    'BUY TICKETS' => 'BUY TICKETS',
+    'Buy ticket with $0.1, and choose numbers for ticket.' => 'Buy ticket with $0.1, and choose numbers for ticket.',
+    'WAIT FOR THE DRAW' => 'WAIT FOR THE DRAW',
+    'Wait for the draw at 15:00 UTC+0 daily.' => 'Wait for the draw at 15:00 UTC+0 daily.',
+    'CHECK FOR PRIZES' => 'CHECK FOR PRIZES',
+    'lottery_how_text' => 'Once the draw is over, come back to this page and check your prize.',
+    
+
+    // promo.blade
+    'PROMO' => 'PROMO',
+    'promo_content_1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'promo_content_2' => 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'join us' => 'join us',
+
+    // referrals.blade
+    'Username' => 'Username',
+    'Email' => 'Email',
+    'Balance' => 'Balance',
+
+    // sport.blade
+    'SPORTS BETTING' => 'SPORTS BETTING',
+    'Coming very soon' => 'Coming very soon',
+    'SIGN UP AND GET' => 'SIGN UP AND GET',
+    'WHEN WE START!' => 'WHEN WE START!',
+    'SING UP' => 'SING UP',
+    'sports_grid_body_cat' => 'Start',
+    'sports_grid_body_title_1' => 'Sunday, 20 November 2022',
+    'sports_grid_body_text_1' => '2022 FIFA World Cup. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    'sports_grid_body_title_2' => 'Sunday, 20 November 2022',
+    'sports_grid_body_text_2' => '2022 FIFA World Cup. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    'sports_grid_body_title_3' => 'Sunday, 20 November 2022',
+    'sports_grid_body_text_3' => '2022 Men\'s Volleyball World Championship',
+    'sports_grid_body_title_4' => 'Sunday, 20 November 2022',
+    'sports_grid_body_text_4' => 'European Summer Sports Championship 2022',
+
+
+    // tourtament.blade
+    'Place' => 'Place',
+    'Player' => 'Player',
+    'Results' => 'Results',
+    'Prize' => 'Prize',
+    'Joined' => 'Joined',
+    'Join' => 'Join',
+
+    // tourtamentSingle.blade
+    'Time left' => 'Time left',
+    'PAID PLACES' => 'PAID PLACES',
+    'RULES OF THE TOURNAMENT' => 'RULES OF THE TOURNAMENT',
+    'tournament_single_page_s_title_1' => 'participants play tournament games and earn points, which are equal to the amount won in these games, that is, each winning $ 1 = 1 point. In tournaments, participants make bets only from the balance of MONEY To participate in the tournament, you have to click on the JOIN button After that, you have to go through the qualifying round - select tournament games (games that participate in the tournament) and play them from the MONEY balance. The total winnings from bets from the MONEY balance must exceed',
+    'tournament_single_page_s_title_2' => '(respectively',
+    'tournament_single_page_s_title_3' => 'points). After accumulating 50 points, these points are taken into account in the rating of this tournament and the next earned points begin to accumulate there.',
+    'tournament_single_page_s_title_4' => 'AT THE END OF THE TOURNAMENT, THE FIRST',
+    'tournament_single_page_s_title_5' => 'PLACES IN THE RANKING WIN. DISTRIBUTION OF THE PRIZE FUND BY PLACES WON IN THE RATING',
+    'back' => 'back',
+    'Tournament games' => 'Tournament games',
+    'Play Now' => 'Play Now',
+    'Payment By Okipays' => 'Payment By Okipays',
+    'Please, use Landscape!' => 'Please, use Landscape!',
+    'MORE GAMES' => 'MORE GAMES',
+
+    // transactions.blade
+    'Transaction ID' => 'Transaction ID',
+    'Remaining Balance' => 'Remaining Balance',
+    'Details' => 'Details',
+    'Date' => 'Date',
+    'Trx' => 'Trx',
+    'ZINO COINS' => 'ZINO COINS',
+    'zinocoins_hero_subtitle' => 'Earn Zino coins while enjoying playing in our online casino',
+    'zinocoins_following_title' => 'Zino coins are credited as bonuses in the following cases',
+    'zinocoins_following_item_description' => 'Upon registration in the amount of 0.5 Zino coin',
+    'zinocoins_following_item_description_1' => 'With a deposit of $100 and up to $300 - 5% of the deposit amount',
+    'zinocoins_following_item_description_2' => 'With a deposit of $300 and up to $500 - 8% of the deposit amount',
+    'zinocoins_following_item_description_3' => 'With a deposit of $500 or more - 10% of the deposit amount',
+    'zinocoins_following_item_description_4' => 'When buying a lottery ticket 0.5 coins per ticket purchased',
+    'zinocoins_following_item_description_5' => 'When participating in tournaments, according to the rules and the prize fund',
+    'What are Zino coins?' => 'What are Zino coins?',
+    'zinocoins_following_item_description_6' => 'Zino coins are bonus user points accumulated in a special balance. The user can exchange these coins in the exchanger for Zino tokens or NFT tokens. You can get bonuses by playing our awesome games, taking part in tournaments and trying your luck in lotteries from the best casino. We strive to be the best online casino in the world and we do everything so that our players have the opportunity to enjoy all the benefits of gambling.',
+    'zinocoins_following_item_description_7' => 'The ability to exchange Zino coins will be available to players in early 2023.',
+    'What are' => 'What are',
+    'Zino Token?' => 'Zino Token?',
+    'what_zinocoins_bottom_description_1' => 'At the beginning of next year, the ZINO token partnership project will be launched. It will be implemented on a separate site and will be a blockchain system platform.',
+    'what_zinocoins_bottom_description_2' => 'Zino tokens are what will enable their owners to be shareholders of the fund, which opens up wide prospects for each participant. Crypto hedge fund from Zino is about liquid cryptocurrencies and real, non-digital assets - futures for gold, currency, securities. This project will include a crypto currency exchange, a wallet, a payment system and other popular services. To appreciate the benefits of Zino tokens, follow our updates!',
+    'Nft Token?' => 'Nft Token?',
+    'what_zinocoins_bottom_description_3' => 'NFT is a unique token, it is like a digital entry in the blockchain registry that gives you the rights to own a unique object of the digital space.',
+    'what_zinocoins_bottom_description_4' => 'NFT is a type of cryptographic token, each copy of which is unique and cannot be exchanged orreplaced by another similar token, although tokens are usually fungible in nature. Each NFT is unique and exists in a single copy, it cannot be divided, and all information about its author, buyer and all transactions with it is securely stored in the blockchain. In other words, an NFT is a digital certificate attached to a unique object. As is the case with any asset, the price of an NFT token for a certain object changes over time, and you may well invest - invest your money in a project at the bottom of the market and sell at the top, thereby earning on the price difference. And we will do our best to help you with this.',
+    'Exchanger' => 'Exchanger',
+    'Very soon' => 'Very soon',
+    'zinocoins_exchanger_description' => 'Here you can apply order to exchange your own Zino coins for Zino tokens and NFT tokens, which will be presented in the gallery. When placing an application, you can choose the NFT token you like for exchange. Our database mainly contains paintings.',
+    
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+    'levels' => '',
+];
